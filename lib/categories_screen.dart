58198cf025776b6330fc3c1eb23c804f0202e034
seoljaehyeok <milkboy2564@naver.com>
2021-01-03 20:19:35 +0900

    Dummy Data 만들고 GridView 생성

---
+++ lib/categories_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/dummy_data.dart';
+import 'package:flutter_meals_app/category_Item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200.0,
+          childAspectRatio: 3 / 2,
+          mainAxisSpacing: 20,
+          crossAxisSpacing: 20),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Korean',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Chinese',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Japanese',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Hamburgers',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Noodles',
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Kimchi',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Bulgogi',
+    color: Colors.grey,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Zapchae',
+    color: Colors.yellow,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Deungalbi',
+    color: Colors.cyan,
+  ),
+];
