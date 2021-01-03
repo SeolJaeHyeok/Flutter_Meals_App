414f7009111d802dc5a929c5626fd6f9612ae923
seoljaehyeok <milkboy2564@naver.com>
2021-01-03 22:16:14 +0900

    카테고리 스크린을 메인 스크린으로 설정

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -5,15 +5,20 @@ import 'package:flutter_meals_app/category_Item.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((catData) => CategoryItem(catData.title, catData.color))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200.0,
-          childAspectRatio: 3 / 2,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeal"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(catData.title, catData.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200.0,
+            childAspectRatio: 3 / 2,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/categories_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,28 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Deli Meal!"),
-      ),
-      body: Center(
-          child: Text(
-        "Navigation Time!",
-        textScaleFactor: 2,
-      )),
+      home: CategoriesScreen(),
     );
   }
 }
